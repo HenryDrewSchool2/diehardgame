5a6ad8d06d6e17d905bd93d1ee810714c5afdf31
HenryDrewSchool2 <henrybishop@drewschool.org>
2025-05-15 09:16:05 -0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,11 @@
+
+
+class Person {
+    String name;
+
+    // Constructor with a parameter
+    public Person(String name) {
+        this.name = name;
+        //System.out.println("Person constructor called for: " + name);
+    }
+}
